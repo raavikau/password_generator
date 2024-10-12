6ca3e8c4c67b6e1f4dcf6acd2d3187941baa4f40
Raavi Kaur <raavi211299@gmail.com>
2024-10-12 18:41:18 -0500

    update the code in main file

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 symbols = ['!', '#', '$', '%', '&', '*', '+', '(', ')']
 
+print("Welcome to the Password Generator!")
 letters_count = int(input("How many letters would you like in your password? "))
 numbers_count = int(input("How many numbers would you like in your password? "))
 symbols_count = int(input("How many symbols would you like in your password? "))
 
 pass_list = []
-for i in range(0, letters_count):
+for _ in range(0, letters_count):
     ran_letter = random.choice(letters)
     pass_list.append(ran_letter)
-for j in range(0, numbers_count):
+for _ in range(0, numbers_count):
     ran_number = random.choice(number)
     pass_list.append(ran_number)
-for k in range(0, symbols_count):
+for _ in range(0, symbols_count):
     ran_symbol = random.choice(symbols)
     pass_list.append(ran_symbol)
 random.shuffle(pass_list)
